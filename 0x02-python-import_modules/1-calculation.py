640835cac4909e62392f2019ecbbdded83ba6768
Konanycode1 <konanytic2020@gmail.com>
2022-09-22 03:29:59 -0700

    code verify

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from calculator_1 import add, sub, mul, div
+
+a = 10
+b = 5
+
+print("{} + {} = {}\n".format(a, b, add(a, b)))
+print("{} - {} = {}\n".format(a, b, sub(a, b)))
+print("{} * {} = {}\n".format(a, b, mul(a, b)))
+print("{} / {} = {}\n".format(a, b, div(a, b)))

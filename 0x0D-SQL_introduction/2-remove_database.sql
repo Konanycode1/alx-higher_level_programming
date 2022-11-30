9c7e09bf96dae20a829c1f12a3163c25d2524fe8
kouame konan abraham <abrahamkonany@gmail.com>
2022-12-01 00:00:27 +0100

    ok

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,4 +1,8 @@
 -- deletes the database hbtn_0c_0 in your MySQL server.
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 -- If the database hbtn_0c_0 doesn’t exist, your script should not fail
 -- You are not allowed to use the SELECT or SHOW statements
 
